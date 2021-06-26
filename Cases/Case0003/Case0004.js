b8699dc9c34f567c685c124fd8d97660c3cffaa4
Carlos Garcia <agzsoftsi@gmail.com>
2021-06-25 22:03:22 -0500

    Case0004

---
+++ Cases/Case0003/Case0004.js
@@ -0,0 +1,20 @@
+// // Case0003 - Consulta aun API 
+// // usar el API https://pokeapi.co/
+// https://pokeapi.co/api/v2/
+//  y mostrar en consola los el id, nombre, tipo, de un poquemon que se ingrese por consola:
+const fetch = require('node-fetch');
+const URL = 'https://pokeapi.co/api/v2/pokemon/';
+
+const pokeInfo = (id) => {
+    fetch(URL+id)
+        .then(response => response.json())
+        .then(data => {
+            console.log('ID: '+ data.id);
+            console.log('Name: '+ data.name);
+            let types = data.types.map(elem => elem.type.name);
+            console.log('Types: ' + types);
+        })
+        .catch(err => console.log(err));
+}
+
+pokeInfo(20);
